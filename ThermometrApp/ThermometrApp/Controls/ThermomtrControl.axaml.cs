c4ebacb77e90dedecbbc9f02dfe40f35a8a8a11b
Fancy <lev2001504@gmail.com>
2023-09-01 18:08:37 +0200

    Почти сделан внешний вид термометра

--- ThermometrApp/ThermometrApp/Controls/ThermomtrControl.axaml.cs
+++ ThermometrApp/ThermometrApp/Controls/ThermomtrControl.axaml.cs
@@ -1,12 +1,23 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 using System;
 
 namespace ThermometrApp.Controls
 {
     public partial class ThermomtrControl : UserControl
     {
+        /// <summary>
+        /// Радиус
+        /// </summary>
+        private const double Radius = 15;
+
+        /// <summary>
+        /// Полуширина трубки термометра
+        /// </summary>
+        private const double PipeHalfWidth = 5;
+
         #region Биндимые свойства
 
         #region Min t
@@ -68,9 +79,55 @@ namespace ThermometrApp.Controls
 
         #endregion
 
+        #region Размер "холста"
+
+        /// <summary>
+        /// Ширина контрола
+        /// </summary>
+        private int _width;
+
+        /// <summary>
+        /// Высота контрола
+        /// </summary>
+        private int _height;
+
+        #endregion
+
+        #region Координаты элементов контрола
+
+        /// <summary>
+        /// Середина контрола (по X)
+        /// </summary>
+        private double _middleX;
+
+        /// <summary>
+        /// Точка - центр окружности
+        /// </summary>
+        private Point _circleCenter = new Point(0, 0);
+
+        /// <summary>
+        /// Низ (по Y) "линейки" с делениями
+        /// </summary>
+        private double _rulerBottomY;
+
+        /// <summary>
+        /// Левая верхняя точка шкалы термометра
+        /// </summary>
+        private double _pipeLeft;
+
+        /// <summary>
+        /// Права нижняя точка шкалы термометра
+        /// </summary>
+        private double _pipeRight;
+
+        #endregion
+
         public ThermomtrControl()
         {
             InitializeComponent();
+
+            // Регистрируем обработчик изменения свойств контрола
+            PropertyChanged += OnPropertyChangedListener;
 
             // Когда меняется значение свойства MinTemperature, вызвать метод HandlMinTemperatureChanged()
             MinTemperatureProperty.Changed.Subscribe(x => HandlMinTemperatureChanged(x.Sender, x.NewValue.GetValueOrDefault<double>()));
@@ -106,6 +163,67 @@ namespace ThermometrApp.Controls
             InvalidateVisual(); // Этот метод заставляет контрол перерисовать себя
         }
 
+        /// <summary>
+        /// Метод вызывается при изменении размеров контрола
+        /// </summary>
+        private void OnResize(Rect bounds)
+        {
+            _width = (int)bounds.Width;
+            _height = (int)bounds.Height;
 
+            _middleX = _width / 2.0;
+
+            _circleCenter = new Point(_middleX, _height - Radius);
+
+            _rulerBottomY = _height - 2 * Radius;
+
+            _pipeLeft = _middleX - PipeHalfWidth;
+            _pipeRight = _middleX + PipeHalfWidth;
+        }
+
+        /// <summary>
+        /// Метод вызывается, когда меняются какие-то свойства контрола
+        /// </summary>
+        private void OnPropertyChangedListener(object sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property.Name.Equals("Bounds")) // Если меняется свойство с именем Bounds (границы контрола)
+            {
+                OnResize((Rect)e.NewValue); // Вызываем OnResize, передав туда новые размеры контрола
+            }
+        }
+
+        /// <summary>
+        /// Метод рисования содержимого контрола
+        /// </summary>
+        public override void Render(DrawingContext context)
+        {
+            base.Render(context);
+
+            // Теперь можно рисовать всякие вещи
+
+            // Рисуем рамку\прямоугольник
+            context.DrawRectangle
+            (
+                new Pen(new SolidColorBrush(Colors.Black)),
+                new Rect(0, 0, _width, _height)
+            );
+
+            // Рисую круг
+            context.DrawEllipse
+            (
+                new SolidColorBrush(Colors.Red),
+                new Pen(new SolidColorBrush(Colors.Black)),
+                _circleCenter,
+                Radius,
+                Radius
+            );
+
+            // Рисуем пустую трубку
+            context.DrawRectangle
+            (
+                new Pen(new SolidColorBrush(Colors.Black)),
+                new Rect(new Point(_pipeLeft, 0), new Point(_pipeRight, _rulerBottomY))
+            );
+        }
     }
 }
